7803502d1f011d07bcfdda0ebb1ec1434e9340f2
Robson Silva <rms2307@outlook.com>
2020-05-28 16:41:24 -0300

    Adding database access-jdbc

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,12 +3,12 @@ package model.entities;
 import java.io.Serializable;
 
 public class Department implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
@@ -62,5 +62,6 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
+	
+	
 }
